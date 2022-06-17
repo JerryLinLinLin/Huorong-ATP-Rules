f82a50454731cc40c36c45996ca8abf5555982d3
JerryLinLinLin <hlin323@wisc.edu>
2022-06-17 17:00:32 -0400

    Update workflow

---
+++ scripts/md_parser.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Author: JerryLinLinLin
+# @Date:   2022-06-17 16:46:42
+# @Last Modified by:   JerryLinLinLin
+# @Last Modified time: 2022-06-17 16:58:11
+
+'''
+This script is to generate markdown doc for all rule sets.
+Genertate each rule description under ./rule/name/
+Generate statistic under ./rule/
+'''
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
